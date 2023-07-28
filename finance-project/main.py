ef75165562a154aa070d9f8caf66c22c57ab94b2
Dealer86 <adicroitor86@yahoo.com>
2023-07-28 18:03:54 +0300

    Added version 0.0.3 and refactored with black

--- finance-project/main.py
+++ finance-project/main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI, Request
 from starlette.responses import JSONResponse
 import logging
 
+from api.crypto import crypto_router
 from api.users import users_router
 from domain_logic.user_repo import NonExistingUserId
 
-app = FastAPI(debug=True,
-              description="A place to track/see and see cryptocurrency prices",
-              title="Crypto Fintech API",
-              version="0.0.1"
-              )
+app = FastAPI(
+    debug=True,
+    description="A place to track/see and see cryptocurrency prices",
+    title="Crypto Fintech API",
+    version="0.0.3",
+)
 
 logging.basicConfig(
     filename="finance.log",
@@ -18,6 +20,7 @@ logging.basicConfig(
 )
 
 app.include_router(users_router)
+app.include_router(crypto_router)
 
 
 @app.exception_handler(NonExistingUserId)
@@ -27,6 +30,7 @@ def return_400(_: Request, e: NonExistingUserId):
 
 if __name__ == "__main__":
     import subprocess
+
     logging.info("Starting webserver...")
     try:
         subprocess.run(["uvicorn", "main:app", "--reload"])
@@ -34,5 +38,3 @@ if __name__ == "__main__":
         logging.warning("Keyboard interrupt.")
     except Exception as e:
         logging.warning("Webserver has stopped. Reason: " + str(e))
-
-
