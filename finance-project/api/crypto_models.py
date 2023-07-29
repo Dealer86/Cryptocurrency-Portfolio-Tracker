0ad729905121e77624bfe435c655b8329339715e
Dealer86 <adicroitor86@yahoo.com>
2023-07-29 16:58:57 +0300

    Added model for adding crypto with name and units to CryptoSchema

--- finance-project/api/crypto_models.py
+++ finance-project/api/crypto_models.py
@@ -2,11 +2,15 @@ from pydantic import BaseModel, Field
 
 
 class CryptoSchema(BaseModel):
-
     name: str = Field(description="Name of cryptocurrency")
     symbol: str = Field(description="Symbol of cryptocurrency")
     price: float = Field(description="Price of cryptocurrency")
     last_updated: str = Field(description="Date of last update")
+    units: float = Field("Number of cryptocurrency")
 
     class Config:
         from_attributes = True
+
+
+class CryptoAdd(BaseModel):
+    name: str = Field(description="Name of cryptocurrency", default="bitcoin")
