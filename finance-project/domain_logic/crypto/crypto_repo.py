346dfa00ff010e52ff40a3c86db6c7353eb105b9
Dealer86 <adicroitor86@yahoo.com>
2023-07-31 14:47:32 +0300

    Added repo with cache

--- finance-project/domain_logic/crypto/crypto_repo.py
+++ finance-project/domain_logic/crypto/crypto_repo.py
@@ -1,57 +1,34 @@
 import sqlite3
 
 from domain_logic.crypto.crypto import Crypto
+from domain_logic.crypto.crypto_persistence_interface import CryptoPersistenceInterface
 
 
 class CryptoRepo:
-    # TODO refactor this code in the persistence layer and implement Repo so that it will manage Crypto objects
-    @classmethod
-    def add_crypto_to_user(cls, user_id: str, cryptocurrency: Crypto):
-        with sqlite3.connect("main_users.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                "CREATE TABLE IF NOT EXISTS crypto "
-                "(user_id TEXT, name TEXT, symbol TEXT, price FLOAT, last_updated TEXT, units FLOAT)"
-            )
-            cursor.execute(
-                "INSERT INTO crypto VALUES (?, ?, ?, ? ,?, ?)",
-                (
-                    user_id,
-                    cryptocurrency.name,
-                    cryptocurrency.symbol,
-                    cryptocurrency.price,
-                    cryptocurrency.last_updated,
-                    cryptocurrency.units,
-                ),
-            )
-            conn.commit()
+    def __init__(self, persistence: CryptoPersistenceInterface):
+        self.__persistence = persistence
+        self.__user_crypto_cache = {}
 
-    @classmethod
-    def get_crypto_for_user(cls, user_id: str) -> list[Crypto]:
-        with sqlite3.connect("main_users.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM crypto WHERE user_id = (?)", (user_id,))
-            list_of_tuple_data = cursor.fetchall()
-            return [
-                Crypto(
-                    name=c[1],
-                    symbol=c[2],
-                    price=c[3],
-                    last_updated=c[4],
-                    units=c[5],
-                )
-                for c in list_of_tuple_data
-            ]
+    def add_crypto_to_user(self, user_id: str, cryptocurrency: Crypto):
+        self.__persistence.add_crypto_to_user(user_id, cryptocurrency)
+        # Clear user's cache when adding new crypto data
+        if user_id in self.__user_crypto_cache:
+            del self.__user_crypto_cache[user_id]
 
-    @classmethod
-    def calculate_total_crypto_value(cls, user_id: str) -> float:
-        with sqlite3.connect("main_users.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT price FROM crypto WHERE user_id = (?)", (user_id,))
-            data = cursor.fetchall()
-            # print(data)
-            result = 0
-            for every_price in data:
-                number = every_price[0]
-                result += number
-            return result
+    def get_crypto_for_user(self, user_id: str) -> list[Crypto]:
+        # Check if user's crypto data is cached
+        if user_id in self.__user_crypto_cache:
+            return self.__user_crypto_cache[user_id]
+        # If not cached, fetch from the database and cache it
+        crypto_list = self.__persistence.get_crypto_for_user(user_id)
+        self.__user_crypto_cache[user_id] = crypto_list
+        return crypto_list
+
+    def calculate_total_crypto_value(self, user_id: str) -> float:
+        # Check if total value is cached
+        if user_id in self.__user_crypto_cache:
+            return sum(crypto.price for crypto in self.__user_crypto_cache[user_id])
+        # If not cached, calculate from the database and cache it
+        total_value = self.__persistence.calculate_total_crypto_value(user_id)
+        self.__user_crypto_cache[user_id] = total_value
+        return total_value
