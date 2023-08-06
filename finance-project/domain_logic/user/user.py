33d0553ac59c65d71053e2f64cfd22859b0ca3c6
Dealer86 <adicroitor86@yahoo.com>
2023-08-06 15:01:10 +0300

    Added setter for username and from and to dict for User class

--- finance-project/domain_logic/user/user.py
+++ finance-project/domain_logic/user/user.py
@@ -17,6 +17,20 @@ class User:
     def username(self):
         return self.__username
 
+    @username.setter
+    def username(self, new_username: str):
+        self.__username = new_username
+
     @property
     def crypto(self) -> list[Crypto]:
         return self.__crypto
+
+    @classmethod
+    def from_dict(cls, d: dict):
+
+        return User(uuid=d["id"], username=d["username"], crypto=[Crypto.from_dict(x) for x in d["crypto"]])
+
+    def to_dict(self):
+        crypto_dict = [Crypto.to_dict(x) for x in self.__crypto]
+        return {"id": str(self.__id), "username": self.__username, "crypto": crypto_dict}
+
