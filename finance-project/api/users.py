e893badc786e3592058e2004456f1265f4ffc3f0
Dealer86 <adicroitor86@yahoo.com>
2023-08-06 16:04:43 +0300

    Added get user and crypto repo functions and inserted them into routers with Depends

--- finance-project/api/users.py
+++ finance-project/api/users.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from api.crypto_models import CryptoSchema, CryptoAdd
 from api.users_models import UserSchema, UserAddSchema
+from configuration.config import set_user_persistence_type, set_crypto_persistence_type
 from domain_logic.crypto.crypto_factory import CryptoFactory
 from domain_logic.crypto.crypto_repo import CryptoRepo
 from domain_logic.user.user_factory import UserFactory
@@ -13,34 +14,40 @@ from persistence.external_crypto_api import ExternalCryptoApi
 from persistence.user_persistence_file import UserPersistenceFile
 
 users_router = APIRouter(prefix="/users")
-file_persistence_users = UserPersistenceFile("main_users.json")
-# user_repo = UserRepo(UserPersistenceSqlite("main_users.db"))
 
-user_repo = UserRepo(file_persistence_users)
-external_api = ExternalCryptoApi()
-# crypto_repo = CryptoRepo(CryptoSqlite("main_users.db"), external_api)
-crypto_repo = CryptoRepo(CryptoPersistenceFile("main_users.json"), external_api)
+
+def get_user_repo():
+    user_persistence_type = set_user_persistence_type("configuration/config.json")
+    user_repo = UserRepo(user_persistence_type)
+    return user_repo
+
+
+def get_crypto_repo():
+    crypto_persistence_type = set_crypto_persistence_type("configuration/config.json")
+    external_api = ExternalCryptoApi()
+    crypto_repo = CryptoRepo(crypto_persistence_type, external_api)
+    return crypto_repo
 
 
 @users_router.get("", response_model=list[UserSchema])
-def get_all_users():
+def get_all_users(user_repo=Depends(get_user_repo)):
     return user_repo.get_all()
 
 
 @users_router.post("", response_model=UserSchema)
-def create_user(user: UserAddSchema):
+def create_user(user: UserAddSchema, user_repo=Depends(get_user_repo)):
     new_user = UserFactory.make(user.username)
     user_repo.add(new_user)
     return new_user
 
 
 @users_router.get("/{user_id}", response_model=UserSchema)
-def get_by_id(user_id: str):
+def get_by_id(user_id: str, user_repo=Depends(get_user_repo)):
     return user_repo.get_by_id(user_id)
 
 
 @users_router.delete("/{user_id}")
-def delete(user_id: str):
+def delete(user_id: str, user_repo=Depends(get_user_repo)):
     try:
         user_repo.delete(user_id)
         return {"status": "ok"}
@@ -49,23 +56,23 @@ def delete(user_id: str):
 
 
 @users_router.put("/{user_id}", response_model=UserSchema)
-def update(user_id: str, username: UserAddSchema):
+def update(user_id: str, username: UserAddSchema, user_repo=Depends(get_user_repo)):
     user_repo.update(user_id, username.username)
     return user_repo.get_by_id(user_id)
 
 
 @users_router.post("/{user_id}/crypto")
-def add_crypto(user_id: str, crypto: CryptoAdd):
+def add_crypto(user_id: str, crypto: CryptoAdd, crypto_repo=Depends(get_crypto_repo)):
     new_crypto = CryptoFactory.make(crypto.name)
     crypto_repo.add_crypto_to_user(user_id, new_crypto)
 
 
 @users_router.get("/{user_id}/crypto", response_model=list[CryptoSchema])
-def get_crypto_for_user(user_id: str):
+def get_crypto_for_user(user_id: str, crypto_repo=Depends(get_user_repo)):
     return crypto_repo.get_crypto_for_user(user_id)
 
 
 @users_router.get("{user_id}/total")
-def get_crypto_total_value_for_user(user_id: str):
+def get_crypto_total_value_for_user(user_id: str, crypto_repo=Depends(get_user_repo)):
     return crypto_repo.calculate_total_crypto_value(user_id)
 
