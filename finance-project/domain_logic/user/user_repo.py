b0696129be5c5862a95832c04511c80c97e9fc09
Dealer86 <adicroitor86@yahoo.com>
2023-08-28 12:57:27 +0300

    Added observer design pattern to UserRepo

--- finance-project/domain_logic/user/user_repo.py
+++ finance-project/domain_logic/user/user_repo.py
@@ -4,18 +4,46 @@ from domain_logic.user.user import User
 from domain_logic.user.user_persistence_interface import UserPersistenceInterface
 from persistence.CryptoPersistenceSqlite import CryptoSqlite
 from persistence.external_crypto_api import ExternalCryptoApi
-
-
+from domain_logic.user.subject import Subject
+from domain_logic.user.observer import Observer
+from domain_logic.user.concrete_logger_observer import ConcreteLoggerObserver
 class NonExistingUserId(Exception):
     pass
 
 
-class UserRepo:
+class UserRepo(Subject):
     def __init__(self, persistence: UserPersistenceInterface):
         self.__user_list = None
         self.__persistence = persistence
         external_api = ExternalCryptoApi()
         self.__crypto_persistence = CryptoRepo(CryptoSqlite("main_users.db"), external_api)
+        self.__observers = []
+        logger_observer = ConcreteLoggerObserver()
+        self.add_observer(logger_observer)
+
+    def add_observer(self, observer: Observer):
+        if observer not in self.__observers:
+            self.__observers.append(observer)
+
+    def remove_observer(self, observer: Observer):
+        if observer in self.__observers:
+            self.__observers.remove(observer)
+
+    def notify_observer_for_adding_user(self, user: User):
+        for o in self.__observers:
+            o.logging_user_added(user)
+
+    def notify_observer_for_updating_username(self, user_id: str, username: str):
+        for o in self.__observers:
+            o.logging_user_update(user_id, username)
+
+    def notify_observer_for_removing_user(self, user_id: str):
+        for o in self.__observers:
+            o.logging_user_removed(user_id)
+
+    def notify_observer_for_get_by_id(self, user_id: str):
+        for o in self.__observers:
+            o.logging_for_get_by_id(user_id)
 
     def get_all(self) -> list[User]:
         self.__check_we_have_users()
@@ -25,6 +53,7 @@ class UserRepo:
         self.__check_we_have_users()
         self.__persistence.add(user)
         self.__user_list.append(user)
+        self.notify_observer_for_adding_user(user)
 
     def get_by_id(self, user_id: str) -> User:
         self.__check_if_user_id_exists(user_id)
@@ -34,22 +63,27 @@ class UserRepo:
                     str(user.id)
                 )
                 try:
+                    self.notify_observer_for_get_by_id(user_id)
                     return User(
                         uuid=user.id, username=user.username, crypto=crypto_list
                     )
                 except Exception as e:
                     print("CEVA NU E IN ORDINE" + str(e))
 
+
     def update(self, user_id: str, username: str):
         self.__check_we_have_users()
         self.__check_if_user_id_exists(user_id)
         self.__persistence.update(user_id, username)
+        self.notify_observer_for_updating_username(user_id, username)
         self.__user_list = self.__persistence.get_all()
+
 
     def delete(self, user_id: str):
         self.__check_we_have_users()
         self.__check_if_user_id_exists(user_id)
         self.__persistence.delete(user_id)
+        self.notify_observer_for_removing_user(user_id)
         self.__user_list = self.__persistence.get_all()
 
     def __check_if_user_id_exists(self, user_id):
