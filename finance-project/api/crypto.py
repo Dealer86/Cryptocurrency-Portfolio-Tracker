f2ad4b0d2e06e3df7f23f41d48c4627b24a81747
Dealer86 <adicroitor86@yahoo.com>
2023-09-04 19:33:44 +0300

    Added router for market cap and called the method directly on the class

--- finance-project/api/crypto.py
+++ finance-project/api/crypto.py
@@ -2,13 +2,12 @@ from fastapi import APIRouter
 
 from api.crypto_models import CryptoSchema
 from api.cryptocurrency_price_history import CryptoCurrencyPriceHistory
+from api.cryptocurrency_market_cap import CryptocurrencyMarketCap
 from domain_logic.crypto.crypto_factory import CryptoFactory
 
 
 crypto_router = APIRouter(prefix="/crypto")
 crypto_factory = CryptoFactory()
-
-crypto_price_history = CryptoCurrencyPriceHistory()
 
 
 @crypto_router.get("{coins_id}", response_model=CryptoSchema)
@@ -21,4 +20,11 @@ def get_cryptocurrency_data(coins_id: str):
 def get_cryptocurrency_price_history(
     coin_id: str, start_date: str = "2023-01-01", end_date: str = "2023-07-28"
 ):
-    return crypto_price_history.get_cryptocurrency_price_history(coin_id, start_date, end_date)
+    return CryptoCurrencyPriceHistory.get_cryptocurrency_price_history(
+        coin_id, start_date, end_date
+    )
+
+
+@crypto_router.get("{coin_id}/market_cap")
+def get_historical_market_cap(coin_id: str, currency: str = "usd", days: int = 7):
+    return CryptocurrencyMarketCap.get_historical_market_cap(coin_id, currency, days)
