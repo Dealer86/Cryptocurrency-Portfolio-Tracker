156b60fe91bb0942d6fdfacef4bcde81d5c80c1c
Dealer86 <adicroitor86@yahoo.com>
2023-09-07 12:45:36 +0300

    Moved observer related methods to the bottom of the code, so we can first see methods that repo manages.

--- finance-project/domain_logic/user/repo.py
+++ finance-project/domain_logic/user/repo.py
@@ -6,44 +6,28 @@ from domain_logic.user.subject import Subject
 from domain_logic.user.observer import Observer
 from domain_logic.user.concrete_logger_observer import ConcreteLoggerObserver
 from configuration.config import set_crypto_persistence_type
+
+
 class NonExistingUserId(Exception):
     pass
+
+
 class UserAlreadyAdded(Exception):
     pass
+
 
 class UserRepo(Subject):
     def __init__(self, persistence: UserPersistenceInterface):
         self.__user_list = None
         self.__persistence = persistence
-        crypto_persistence_type = set_crypto_persistence_type("configuration/config.json")
+        crypto_persistence_type = set_crypto_persistence_type(
+            "configuration/config.json"
+        )
         self.__crypto_persistence = CryptoRepo(crypto_persistence_type)
         self.__observers = []
         logger_observer = ConcreteLoggerObserver()
         self.add_observer(logger_observer)
 
-    def add_observer(self, observer: Observer):
-        if observer not in self.__observers:
-            self.__observers.append(observer)
-
-    def remove_observer(self, observer: Observer):
-        if observer in self.__observers:
-            self.__observers.remove(observer)
-
-    def notify_observer_for_adding_user(self, user: User):
-        for o in self.__observers:
-            o.logging_user_added(user)
-
-    def notify_observer_for_updating_username(self, user_id: str, username: str):
-        for o in self.__observers:
-            o.logging_user_update(user_id, username)
-
-    def notify_observer_for_removing_user(self, user_id: str):
-        for o in self.__observers:
-            o.logging_user_removed(user_id)
-
-    def notify_observer_for_get_by_id(self, user_id: str):
-        for o in self.__observers:
-            o.logging_for_get_by_id(user_id)
 
     def get_all(self) -> list[User]:
         self.__check_we_have_users()
@@ -52,7 +36,9 @@ class UserRepo(Subject):
     def add(self, user: User):
         self.__check_we_have_users()
         if user.username in [u.username for u in self.__persistence.get_all()]:
-            raise UserAlreadyAdded(f"User with username {user.username} is already taken. Try another username")
+            raise UserAlreadyAdded(
+                f"User with username {user.username} is already taken. Try another username"
+            )
         self.__persistence.add(user)
         self.__user_list.append(user)
         self.notify_observer_for_adding_user(user)
@@ -79,12 +65,35 @@ class UserRepo(Subject):
         self.notify_observer_for_updating_username(user_id, username)
         self.__user_list = self.__persistence.get_all()
 
-
     def delete(self, user_id: str):
         self.__check_if_user_id_exists(user_id)
         self.__persistence.delete(user_id)
         self.notify_observer_for_removing_user(user_id)
         self.__user_list = self.__persistence.get_all()
+
+    def add_observer(self, observer: Observer):
+        if observer not in self.__observers:
+            self.__observers.append(observer)
+
+    def remove_observer(self, observer: Observer):
+        if observer in self.__observers:
+            self.__observers.remove(observer)
+
+    def notify_observer_for_adding_user(self, user: User):
+        for o in self.__observers:
+            o.logging_user_added(user)
+
+    def notify_observer_for_updating_username(self, user_id: str, username: str):
+        for o in self.__observers:
+            o.logging_user_update(user_id, username)
+
+    def notify_observer_for_removing_user(self, user_id: str):
+        for o in self.__observers:
+            o.logging_user_removed(user_id)
+
+    def notify_observer_for_get_by_id(self, user_id: str):
+        for o in self.__observers:
+            o.logging_for_get_by_id(user_id)
 
     def __check_if_user_id_exists(self, user_id):
         self.__check_we_have_users()
