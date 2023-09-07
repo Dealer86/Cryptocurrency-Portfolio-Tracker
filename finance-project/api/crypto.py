32313307273ab400783a85fd8c419c6a1c905fa5
Dealer86 <adicroitor86@yahoo.com>
2023-09-07 13:38:32 +0300

    Moved CryptoCurrencyPriceHistory and CryptoCurrencyMarketCap classes from api layer into the domain layer/business layer. Joining them into a single class named CryptoInfo that now stores information about both of them, so I can use a single class in crypto router when dealing with get requests for that purpose.

--- finance-project/api/crypto.py
+++ finance-project/api/crypto.py
@@ -1,18 +1,16 @@
 from fastapi import APIRouter
 
 from api.models.crypto_models import CryptoSchema
-from api.cryptocurrency_price_history import CryptoCurrencyPriceHistory
-from api.cryptocurrency_market_cap import CryptocurrencyMarketCap
+from domain_logic.crypto.crypto_info import CryptoInfo
 from domain_logic.crypto.crypto_factory import CryptoFactory
 
 
 crypto_router = APIRouter(prefix="/crypto")
-crypto_factory = CryptoFactory()
 
 
 @crypto_router.get("{coins_id}", response_model=CryptoSchema)
 def get_cryptocurrency_data(coins_id: str):
-    crypto = crypto_factory.make(coins_id)
+    crypto = CryptoFactory.make(coins_id)
     return crypto
 
 
@@ -20,11 +18,11 @@ def get_cryptocurrency_data(coins_id: str):
 def get_cryptocurrency_price_history(
     coin_id: str, start_date: str = "2023-01-01", end_date: str = "2023-07-28"
 ):
-    return CryptoCurrencyPriceHistory.get_cryptocurrency_price_history(
+    return CryptoInfo.get_cryptocurrency_price_history(
         coin_id, start_date, end_date
     )
 
 
 @crypto_router.get("{coin_id}/market_cap")
 def get_historical_market_cap(coin_id: str, currency: str = "usd", days: int = 7):
-    return CryptocurrencyMarketCap.get_historical_market_cap(coin_id, currency, days)
+    return CryptoInfo.get_historical_market_cap(coin_id, currency, days)
