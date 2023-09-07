8a2c24389dc6576fee326c60f44ee5cfb2270403
Dealer86 <adicroitor86@yahoo.com>
2023-09-07 16:15:37 +0300

    Added abstract method update for interface Observer and removed older methods, because now the concrete Observers will use only one method.

--- finance-project/domain_logic/user/observer.py
+++ finance-project/domain_logic/user/observer.py
@@ -1,21 +1,8 @@
 from abc import ABC, abstractmethod
 
-from domain_logic.user.user import User
-
 
 class Observer(ABC):
     @abstractmethod
-    def logging_user_added(self, user: User):
+    def update(self, message: str):
         pass
 
-    @abstractmethod
-    def logging_user_removed(self, user_id: str):
-        pass
-
-    @abstractmethod
-    def logging_user_update(self, user_id: str, username: str):
-        pass
-
-    @abstractmethod
-    def logging_for_get_by_id(self, user_id: str):
-        pass
