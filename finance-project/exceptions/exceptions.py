81da05187623f5e8fde39aaf64819fc6c1368e14
Dealer86 <adicroitor86@yahoo.com>
2023-09-08 15:32:51 +0300

    Moved exceptions into their own folder.

---
+++ finance-project/exceptions/exceptions.py
@@ -0,0 +1,22 @@
+class UsernameAlreadyExistsException(Exception):
+    pass
+
+
+class UserFileError(Exception):
+    pass
+
+
+class InvalidUsername(Exception):
+    pass
+
+
+class UserNotFound(Exception):
+    pass
+
+
+class UserAlreadyAdded(Exception):
+    pass
+
+
+class InvalidCoinId(Exception):
+    pass
