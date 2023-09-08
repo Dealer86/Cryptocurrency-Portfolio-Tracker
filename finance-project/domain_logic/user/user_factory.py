c80587a927b45d95495277120bffd52537de4a00
Dealer86 <adicroitor86@yahoo.com>
2023-09-08 15:27:19 +0300

    Optimizing imports after moving exceptions into their own folder. Added extra validations for username

--- finance-project/domain_logic/user/user_factory.py
+++ finance-project/domain_logic/user/user_factory.py
@@ -1,17 +1,15 @@
-
 import uuid
 
+from exceptions.exceptions import InvalidUsername
 from domain_logic.user.user import User
-
-
-class InvalidUsername(Exception):
-    pass
 
 
 class UserFactory:
     @classmethod
     def make(cls, username: str) -> User:
         cls.__validate_username(username)
+        cls.__validate_is_alpha_numeric(username)
+        cls.__validate_no_prohibited_words(username)
         user_id = uuid.uuid4()
 
         return User(user_id, username)
@@ -21,3 +19,15 @@ class UserFactory:
         if not (4 < len(username) < 20):
             raise InvalidUsername("Username must be between 4 and 20 chars")
 
+    @classmethod
+    def __validate_is_alpha_numeric(cls, username: str):
+        if not username.isalnum():
+            raise InvalidUsername("Username must be alpha numeric!")
+
+    @classmethod
+    def __validate_no_prohibited_words(cls, username: str):
+        prohibited_words = ["admin", "root", "password", "superuser", "guest"]
+        if username.lower() in prohibited_words:
+            raise InvalidUsername(
+                "Username must not contain prohibited words like admin, etc."
+            )
