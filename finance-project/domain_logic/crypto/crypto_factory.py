64cd60e187817cca81b264129575876f3dc766f5
Dealer86 <adicroitor86@yahoo.com>
2023-09-08 15:58:05 +0300

    Added logging and exception handling.

--- finance-project/domain_logic/crypto/crypto_factory.py
+++ finance-project/domain_logic/crypto/crypto_factory.py
@@ -1,5 +1,5 @@
 import requests
-
+import logging
 from domain_logic.crypto.crypto import Crypto
 from exceptions.exceptions import InvalidCoinId
 
@@ -10,10 +10,17 @@ class CryptoFactory:
         base_url = "https://api.coingecko.com/api/v3"
         endpoint = f"/coins/{coins_id}"
         try:
+            logging.info(
+                "CryptoFactory executing make command, making a get request to Coingecko url..."
+            )
             response = requests.get(base_url + endpoint)
             if response.status_code == 200:
+                logging.info("CryptoFactory successfully executed the get request.")
                 data = response.json()
-                if data is not None:  # Check if data is not None
+                if data is not None:
+                    logging.info(
+                        f"CryptoFactory successfully created a Crypto object for coin with ticker {coins_id}."
+                    )
                     return Crypto(
                         name=data["name"],
                         price=data["market_data"]["current_price"]["usd"],
@@ -22,11 +29,15 @@ class CryptoFactory:
                         units=0,
                     )
                 else:
-                    raise InvalidCoinId(
-                        "Coin Id does not exist. This is from Crypto obj return!"
+                    logging.warning(
+                        f"CryptoFactory could not retrieve data for {coins_id}. Data is None"
                     )
+                    raise InvalidCoinId(f"Could not retrieve data for {coins_id}!")
             elif response.status_code == 404:
-                raise InvalidCoinId("Coin Id does not exist!")
-
+                logging.warning(
+                    f"CryptoFactory could not retrieve data for {coins_id},"
+                    f"it doesn't exist! Try another coin id/ticker."
+                )
+                raise InvalidCoinId(f"Coin {coins_id} does not exist! Try another one.")
         except Exception as e:
-            raise InvalidCoinId("Coin Id does not exist!")
+            raise InvalidCoinId(str(e))
