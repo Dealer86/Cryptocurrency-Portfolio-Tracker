4a0be2691bb657ae23a9a5a8935e65e1a917d385
Dealer86 <adicroitor86@yahoo.com>
2023-09-11 12:37:55 +0300

    Added global exception handler for invalid database selection.

--- finance-project/main.py
+++ finance-project/main.py
@@ -1,10 +1,9 @@
 import logging
-
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from starlette.responses import JSONResponse
 from api.crypto import crypto_router
 from api.users import users_router
-
+from configuration.config import InvalidDataBase
 
 app = FastAPI(
     debug=True,
@@ -21,6 +20,12 @@ logging.basicConfig(
 
 app.include_router(users_router)
 app.include_router(crypto_router)
+
+
+@app.exception_handler(InvalidDataBase)
+def return_invalid_database(_: Request, e: InvalidDataBase):
+    logging.warning(f"Invalid database selected. Reason: {str(e)}")
+    return JSONResponse(status_code=404, content=str(e))
 
 
 if __name__ == "__main__":
