e1b07b5204ff83d438f43d6dfd799f9f21fda106
Dealer86 <adicroitor86@yahoo.com>
2023-09-11 19:13:29 +0300

    Moved database file into persistence layer and optimized imports after modules rename.

--- finance-project/configuration/config.py
+++ finance-project/configuration/config.py
@@ -1,7 +1,7 @@
 import json
 
-from persistence.CryptoPersistenceSqlite import CryptoSqlite
-from persistence.UserPersistenceSqlite import UserPersistenceSqlite
+from persistence.crypto_persistence_sqlite import CryptoSqlite
+from persistence.user_persistence_sqlite import UserPersistenceSqlite
 from persistence.crypto_persistence_file import CryptoPersistenceFile
 from persistence.user_persistence_file import UserPersistenceFile
 
@@ -15,9 +15,9 @@ def set_user_persistence_type(file_path: str):
         data = file.read()
         user_config_choice = json.loads(data)
         if user_config_choice.get("persistence") == "json":
-            return UserPersistenceFile("main_users.json")
+            return UserPersistenceFile("persistence/main_users.json")
         elif user_config_choice.get("persistence") == "sqlite":
-            return UserPersistenceSqlite("main_users.db")
+            return UserPersistenceSqlite("persistence/main_users.db")
         else:
             raise InvalidDataBase(
                 "Config must be json or sqlite, be sure it is properly specified in config.json"
@@ -29,9 +29,9 @@ def set_crypto_persistence_type(file_path: str):
         data = file.read()
         user_config_choice = json.loads(data)
         if user_config_choice.get("persistence") == "json":
-            return CryptoPersistenceFile("main_users.json")
+            return CryptoPersistenceFile("persistence/main_users.json")
         elif user_config_choice.get("persistence") == "sqlite":
-            return CryptoSqlite("main_users.db")
+            return CryptoSqlite("persistence/main_users.db")
         else:
             raise InvalidDataBase(
                 "Config must be json or sqlite, be sure it is properly specified in config.json"
