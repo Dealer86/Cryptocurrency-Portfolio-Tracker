901977f56bd80ed64c45ac66ea4cc42423cf709c
Dealer86 <adicroitor86@yahoo.com>
2023-10-30 20:29:00 +0200

    Added UserPersistenceFile Test

--- tests/persistence/test_user_file.py
+++ tests/persistence/test_user_file.py
@@ -5,11 +5,11 @@ from persistence.user_persistence_file import UserPersistenceFile
 
 class UserPersistenceFileTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.user_persistence_sqlite = UserPersistenceFile("user_test.json")
+        self.user_persistence_file = UserPersistenceFile("user_test.json")
 
     def test_get_all_gets_empty_list_if_database_is_empty(self):
         # execution
-        all_users = self.user_persistence_sqlite.get_all()
+        all_users = self.user_persistence_file.get_all()
 
         # assertion
         self.assertEqual([], all_users)
@@ -20,8 +20,8 @@ class UserPersistenceFileTestCase(unittest.TestCase):
         self.new_user = UserFactory.make(username)
 
         # execution
-        self.user_persistence_sqlite.add(self.new_user)
-        actual_new_user = [u.username for u in self.user_persistence_sqlite.get_all()]
+        self.user_persistence_file.add(self.new_user)
+        actual_new_user = [u.username for u in self.user_persistence_file.get_all()]
 
         # assertion
         self.assertIn(self.new_user.username, actual_new_user)
@@ -30,11 +30,11 @@ class UserPersistenceFileTestCase(unittest.TestCase):
         # set up
         username = "random1"
         self.new_user = UserFactory.make(username)
-        self.user_persistence_sqlite.add(self.new_user)
+        self.user_persistence_file.add(self.new_user)
 
         # execution
-        self.user_persistence_sqlite.delete(str(self.new_user.id))
-        all_users = self.user_persistence_sqlite.get_all()
+        self.user_persistence_file.delete(str(self.new_user.id))
+        all_users = self.user_persistence_file.get_all()
 
         # assertion
         self.assertNotIn(self.new_user.username, all_users)
@@ -43,12 +43,12 @@ class UserPersistenceFileTestCase(unittest.TestCase):
         # set up
         username = "random2"
         self.new_user = UserFactory.make(username)
-        self.user_persistence_sqlite.add(self.new_user)
+        self.user_persistence_file.add(self.new_user)
 
         # execution
         actual_new_username = "newname"
-        self.user_persistence_sqlite.update(str(self.new_user.id), actual_new_username)
-        all_usernames = [u.username for u in self.user_persistence_sqlite.get_all()]
+        self.user_persistence_file.update(str(self.new_user.id), actual_new_username)
+        all_usernames = [u.username for u in self.user_persistence_file.get_all()]
 
         # assertion
         self.assertIn(actual_new_username, all_usernames)
